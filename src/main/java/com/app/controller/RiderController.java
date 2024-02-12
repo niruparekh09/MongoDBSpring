304b27e32aea6a777e722817cf2da980cd129a96
Nirav Parekh <niruparekh09@gmail.com>
2024-02-12 19:38:21 +0530

    Added @DBRef

--- src/main/java/com/app/controller/RiderController.java
+++ src/main/java/com/app/controller/RiderController.java
@@ -2,6 +2,8 @@ package com.app.controller;
 
 import com.app.dto.APIResponse;
 import com.app.enities.Rider;
+import com.app.enities.Team;
+import com.app.repository.TeamRepo;
 import com.app.service.RiderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,11 +20,18 @@ public class RiderController {
     @Autowired
     RiderService riderService;
 
+    @Autowired
+    TeamRepo teamRepo;
+
     @PostMapping("/add")
     ResponseEntity<?> addRider(@RequestBody @Valid Rider newRider) {
+        /*I am basically removing the need of user to add any ID from frontend, rather I am taking the team name
+         and fetching the ID by myself and replacing old Team reference inside newRider to new Team reference which
+         also has ID added to it.*/
         try {
+            Team team = newRider.getTeamName();
+            newRider.setTeamName(teamRepo.findTeamByName(team.getName()));
             Rider newRiderInfo = riderService.AddRider(newRider);
-            System.out.println("ID after saving: " + newRiderInfo.getId());
             return ResponseEntity.status(HttpStatus.CREATED).body(new APIResponse(newRiderInfo.toString()));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse(e.getMessage()));
@@ -44,6 +53,16 @@ public class RiderController {
         try {
             Rider riderInfo = riderService.getRiderDetails(id);
             return ResponseEntity.status(HttpStatus.CREATED).body(new APIResponse(riderInfo.toString()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse(e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<?> deleteRider(@PathVariable String id) {
+        try {
+            String deletedRider = riderService.deleteARider(id);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new APIResponse(deletedRider));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse(e.getMessage()));
         }
--- src/main/java/com/app/enities/Rider.java
+++ src/main/java/com/app/enities/Rider.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Document(collection = "Rider")
