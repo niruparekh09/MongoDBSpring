43e8e83b7eddde00c0867f9c0a5b8a45832c9d97
Nirav Parekh <niruparekh09@gmail.com>
2024-02-12 14:53:53 +0530

    Added Global Exception Handler.

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -13,7 +13,6 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    //Method to configure ModelMapper bean as a spring bean (to be managed by SC)
     @Bean // equivalent to <bean id ..../> in xml file
     public ModelMapper mapper() {
         ModelMapper modelMapper = new ModelMapper();
